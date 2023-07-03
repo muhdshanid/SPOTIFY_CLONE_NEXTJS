a862f0c03d92ccd6a6628e30436a94385d8e4aea
muhdshanid <shanishanid91@gmail.com>
2023-07-03 23:03:53 +0530

    components created for layout

---
+++ src/components/Box.tsx
@@ -0,0 +1,17 @@
+import { twMerge } from "tailwind-merge"
+
+type BoxProps = {
+    children: React.ReactNode,
+    className?: string
+}
+const Box = ({children, className}: BoxProps) => {
+  return (
+    <div className={twMerge(`
+    bg-neutral-900 rounded-lg h-fit w-full
+    `,className)}>
+        {children}
+    </div>
+  )
+}
+
+export default Box
