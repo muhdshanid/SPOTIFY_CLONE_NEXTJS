6843ce2bce943bc24d1ed8d4f02efa6b72698254
muhdshanid <shanishanid91@gmail.com>
2023-07-04 22:36:37 +0530

    home page completed

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,7 +1,11 @@
+import { getSongs } from "@/actions/actions";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
-export default function Home() {
+export const revalidate = 0
+export default async function Home() {
+  const songs = await getSongs()
   return (
   <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden
   overflow-y-auto">
@@ -19,7 +23,7 @@ export default function Home() {
       <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
     </div>
     <div>
-      List of Songs!
+          <PageContent songs={songs}/>
     </div>
    </div>
   </div>
