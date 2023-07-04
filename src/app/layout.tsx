ceb938cfd9381e63fe14ce11075131b01572c2eb
muhdshanid <shanishanid91@gmail.com>
2023-07-04 22:41:17 +0530

    error and loading page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import { Figtree } from 'next/font/google'
 import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToastProvider from '@/providers/ToastProvider'
+import { getSongsById } from '@/actions/actions'
+import { Player } from '@/components/Player'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -10,19 +15,25 @@ export const metadata = {
   description: 'Listen to music',
 }
 
-export default function RootLayout({
+export const revalidate = 0
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsById()
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToastProvider/>
         <SupabaseProvider>
-
-        <Sidebar>
+          <UserProvider>
+            <ModalProvider/>
+        <Sidebar userSongs={userSongs}>
         {children}
         </Sidebar>
+        <Player/>
+          </UserProvider>
         </SupabaseProvider>
         </body>
     </html>
