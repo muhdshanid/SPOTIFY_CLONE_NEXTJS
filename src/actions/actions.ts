64bc8adf1c17fd0f743e47a889bf724f89e49848
muhdshanid <shanishanid91@gmail.com>
2023-07-04 22:32:02 +0530

    action functions completed

---
+++ src/actions/actions.ts
@@ -0,0 +1,95 @@
+import { Song } from "@/types/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from 'next/headers'
+
+
+export const getSongs = async (): Promise<Song[]> => {
+    
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    const {data, error} = await supabase.from("songs")
+    .select("*").order('created_at',{ascending: false})
+
+    if(error){
+        console.log(error.message);
+        
+    }
+
+    return (data as any || [])
+}
+export const getLikedSongs = async (): Promise<Song[]> => {
+
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    const {data: {
+        session
+    }} = await supabase.auth.getSession()
+    
+
+    const {data, error} = await supabase.from("liked_songs")
+    .select("*, songs(*)").eq('user_id', session?.user.id).order('created_at',{ascending: false})
+
+    if(error){
+        console.log(error.message);
+        return []
+    }
+
+    if(!data) {
+        return []
+    }
+
+    return data.map(item => ({
+        ...item.songs
+    }))
+}
+
+export const getSongsById = async (): Promise<Song[]> =>
+{
+    
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    const {data: sessionData, error: sessionError} = await supabase.auth.getSession()
+    if(sessionError){
+        console.log(sessionError.message)
+        return []
+        
+    }
+    const {data, error} = await supabase.from("songs")
+    .select("*").eq('user_id', sessionData.session?.user.id).order('created_at',{ascending: false})
+
+    if(error){
+        console.log(error.message);
+        
+    }
+
+    return (data as any || [])
+}
+export const getSongsByTitle = async (title: string): Promise<Song[]> =>
+{
+
+    if(!title){
+        const allSongs = await getSongs()
+        return allSongs
+    }
+    
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    const {data, error} = await supabase.from("songs")
+    .select("*").ilike("title", `%${title}%`)
+    .order('created_at',{ascending: false})
+
+    if(error){
+        console.log(error.message);
+        
+    }
+
+    return (data as any || [])
+}
