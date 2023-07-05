a16dda87bdcae815b3c546e2169be30a0d80adbd
muhdshanid <shanishanid91@gmail.com>
2023-07-05 17:02:09 +0530

    stripe subscription functions completed

--- src/libs/stripe.ts
+++ src/libs/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 
-export const strip = new Stripe(
+export const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY ?? "",
     {
         apiVersion: "2022-11-15",
